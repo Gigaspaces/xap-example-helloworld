8c6d875d31716afaef32a66e14b6eec6b38dc45a
barakbo <barakbo@eb64e737-3616-4df0-8941-5ee2ae88103d>
2014-07-24 10:37:21 +0000

    clean LOOKUPGROUPS after example is running -- this way if setenv.bat is updated between runs the new value will be use.
    git-svn-id: svn://pc-lab14/SVN/xap/trunk/examples@189423 eb64e737-3616-4df0-8941-5ee2ae88103d

--- examples/web/plain/build.bat
+++ examples/web/plain/build.bat
@@ -3,3 +3,4 @@
 @call "%~dp0\..\..\..\bin\setenv.bat"
 
 @%JAVACMD% %LOOKUP_GROUPS_PROP% -classpath %SIGAR_JARS%;%GS_JARS%;%ANT_JARS%;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
+set LOOKUPGROUPS=""
