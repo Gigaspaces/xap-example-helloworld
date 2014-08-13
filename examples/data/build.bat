2a26c56baba2b4dbac463fad0ecda42da569a49f
barakbo <barakbo@eb64e737-3616-4df0-8941-5ee2ae88103d>
2014-08-13 10:42:27 +0000

    Fix GS-11938 All java examples' build.bat script should not reset LOOKUPGROUPS
    git-svn-id: svn://pc-lab14/SVN/xap/trunk/examples@191447 eb64e737-3616-4df0-8941-5ee2ae88103d

--- examples/data/build.bat
+++ examples/data/build.bat
@@ -3,4 +3,3 @@
 @call "%~dp0\..\..\bin\setenv.bat"
 
 @%JAVACMD% %LOOKUP_LOCATORS_PROP% %LOOKUP_GROUPS_PROP% -classpath %SIGAR_JARS%;%GS_JARS%;%ANT_JARS%;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
-set LOOKUPGROUPS=
