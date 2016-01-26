f926fa0565c782aa12986f8fc4c15851f6460be0
Yohana Khoury <yohana@yohanakhoury.com>
2016-01-26 12:06:05 +0200

    Update build.sh
    removed JSHOMEDIR env var

--- examples/helloworld/build.sh
+++ examples/helloworld/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-JSHOMEDIR=`dirname $0`/../..; export JSHOMEDIR
-. ${JSHOMEDIR}/bin/setenv.sh
+. ../../bin/setenv.sh
 
 "$JAVACMD" ${LOOKUP_GROUPS_PROP} -classpath "${SIGAR_JARS}":"${GS_JARS}":"${ANT_JARS}":"${JAVA_HOME}/lib/tools.jar" org.apache.tools.ant.Main $1
