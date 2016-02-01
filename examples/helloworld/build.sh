7d75776c965e9b71c7af19ec095a8a53476732d6
Evgeny Fisher <evgenyf@gigaspaces.com>
2016-02-01 16:24:13 +0200

    added usage new environment variable XAP_OPTIONS

--- examples/helloworld/build.sh
+++ examples/helloworld/build.sh
@@ -2,4 +2,4 @@
 
 . `dirname $0`/../../bin/setenv.sh
 
-"$JAVACMD" ${LOOKUP_GROUPS_PROP} -classpath "${SIGAR_JARS}":"${GS_JARS}":"${ANT_JARS}":"${JAVA_HOME}/lib/tools.jar" org.apache.tools.ant.Main $1
+"$JAVACMD" ${XAP_OPTIONS} -classpath "${SIGAR_JARS}":"${GS_JARS}":"${ANT_JARS}":"${JAVA_HOME}/lib/tools.jar" org.apache.tools.ant.Main $1
