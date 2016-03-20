86ba7d8afbd1cf41307ed648cd5131dc326b38d0
tamirt <tamirt@gigaspaces,com>
2016-03-20 15:47:04 +0200

    GS-12768 fixed log message when no arguments were given

--- examples/helloworld/build.sh
+++ examples/helloworld/build.sh
@@ -7,7 +7,21 @@ if [ "${M2_HOME}" = "" ] ; then
     M2_HOME="${XAP_HOME}/tools/maven/apache-maven-3.2.5"; export M2_HOME
 fi
 
-if [ $1 = "clean" ]; then
+if [ -z "$1" ] || ([ $1 != "clean" ] && [ $1 != "compile" ] && [ $1 != "package" ] && [ $1 != "deploy-processor" ] && [ $1 != "run-feeder" ] && [ $1 != "intellij" ]); then
+  echo ""
+  echo "Error: Invalid input command $1 "
+  echo ""
+  echo "The available commands are:"
+  echo ""
+  echo "clean                    --> Cleans all output dirs"
+  echo "compile                  --> Builds all; don't create JARs"
+  echo "package                  --> Builds the distribution"
+  echo "deploy-processor         --> Deploys the processor onto the service grid"
+  echo "run-feeder               --> Starts the feeder"
+  echo "intellij                 --> Creates run configuration for IntelliJ IDE"
+  echo
+
+elif [ $1 = "clean" ]; then
   (cd $DIR_NAME;
   ${M2_HOME}/bin/mvn clean; )
 
@@ -31,18 +45,4 @@ elif [ $1 = "run-feeder" ]; then
 elif [ $1 = "intellij" ]; then
   cp -r $DIR_NAME/runConfigurations $DIR_NAME/.idea
   echo "Run configurations for IntelliJ IDE created successfully"
-
-else
-  echo ""
-  echo "Error: Invalid input command $1 "
-  echo ""
-  echo "The available commands are:"
-  echo ""
-  echo "clean                    --> Cleans all output dirs"
-  echo "compile                  --> Builds all; don't create JARs"
-  echo "package                  --> Builds the distribution"
-  echo "deploy-processor         --> Deploys the processor onto the service grid"
-  echo "run-feeder               --> Starts the feeder"
-  echo "intellij                 --> Creates run configuration for IntelliJ IDE"
-  echo ""
 fi
